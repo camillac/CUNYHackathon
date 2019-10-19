c2db0be758ba9d08a9f8500e20486bd647d94344
camillac <camilla.cc19@gmail.com>
2019-10-18 23:33:43 -0400

    removing turn funcs

--- test.py
+++ test.py
@@ -20,15 +20,15 @@ def load_image(i):
     'load an image from the data directory with per pixel alpha transparency.'
     return pygame.image.load(os.path.join(".", i)).convert_alpha()
 
-trash = load_image("turtle.png")
-turtle = load_image("terrain1.png")
+turtle = load_image("turtle.png")
+trash = load_image("terrain1.png")
 
 # create a mask for each of them.
-trash_mask = pygame.mask.from_surface(trash, 50)
 turtle_mask = pygame.mask.from_surface(turtle, 50)
+trash_mask = pygame.mask.from_surface(trash, 50)
 
-trash_rect = trash.get_rect()
 turtle_rect = turtle.get_rect()
+trash_rect = trash.get_rect()
 
 # a message for if the balloon hits the terrain.
 afont = pygame.font.Font(None, 16)
@@ -45,23 +45,23 @@ while going:
     # if e.type == pygame.KEYDOWN:
     #     # move the balloon around, depending on the keys.
     if keys[K_LEFT]:
-        turtle_rect.x -= 1
+        trash_rect.x -= 1
         # turtle = load_image("turtle2.png")
     if keys[K_RIGHT]:
-        turtle_rect.x += 1
+        trash_rect.x += 1
 
     if keys[K_UP]:
-        turtle_rect.y -= 1
+        trash_rect.y -= 1
     if keys[K_DOWN]:
-        turtle_rect.y += 1
+        trash_rect.y += 1
 
     # see how far the balloon rect is offset from the terrain rect.
-    bx, by = (turtle_rect[0], turtle_rect[1])
-    offset_x = bx - trash_rect[0]
-    offset_y = by - trash_rect[1]
+    bx, by = (trash_rect[0], trash_rect[1])
+    offset_x = bx - turtle_rect[0]
+    offset_y = by - turtle_rect[1]
 
     #print bx, by
-    overlap = trash_mask.overlap(turtle_mask, (offset_x, offset_y))
+    overlap = turtle_mask.overlap(trash_mask, (offset_x, offset_y))
 
     #
     last_bx, last_by = bx, by
@@ -69,13 +69,13 @@ while going:
 
     # draw the background color, and the terrain.
     screen.fill((7,176,157))
-    screen.blit(trash, (0,0))
+    screen.blit(turtle, (0,0))
 
     # draw the balloon.
-    screen.blit(turtle, (turtle_rect[0], turtle_rect[1]) )
+    screen.blit(trash, (trash_rect[0], trash_rect[1]) )
 
     # draw the balloon rect, so you can see where the bounding rect would be.
-    pygame.draw.rect(screen, (0,255,0), turtle_rect, 1)
+    pygame.draw.rect(screen, (0,255,0), trash_rect, 1)
 
 
     # see if there was an overlap of pixels between the balloon
