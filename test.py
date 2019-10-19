0e9751807525749d4b9b55d1756bded18f9aae95
camillac <camilla.cc19@gmail.com>
2019-10-18 20:33:40 -0400

    turtle logo

--- test.py
+++ test.py
@@ -1,22 +1,22 @@
 # import the pygame module, so you can use it
 import pygame
- 
+
 # define a main function
 def main():
-     
+
     # initialize the pygame module
     pygame.init()
     # load and set the logo
-    logo = pygame.image.load("logo32x32.png")
+    logo = pygame.image.load("turtle.png")
     pygame.display.set_icon(logo)
     pygame.display.set_caption("minimal program")
-     
+
     # create a surface on screen that has the size of 240 x 180
-    screen = pygame.display.set_mode((240,180))
-     
+    screen = pygame.display.set_mode((750,750))
+
     # define a variable to control the main loop
     running = True
-     
+
     # main loop
     while running:
         # event handling, gets all event from the event queue
@@ -25,8 +25,8 @@ def main():
             if event.type == pygame.QUIT:
                 # change the value to False, to exit the main loop
                 running = False
-     
-     
+
+
 # run the main function only if this module is executed as the main script
 # (if you import this as a module then nothing is executed)
 if __name__=="__main__":
