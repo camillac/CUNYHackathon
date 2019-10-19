f5bf8f52ef1f83761a81a20a70207b5851a88af0
Shafali Gupta <shafaligupta31@gmail.com>
2019-10-19 14:35:29 -0400

    did a thing

--- testing_stuff/testing_after.py
+++ testing_stuff/testing_after.py
@@ -10,13 +10,18 @@ logo = pygame.image.load("../turtle_left.png")
 pygame.display.set_icon(logo)
 pygame.display.set_caption("Turtles In Trash")
 
+keystroke = 0
+LEFT = 1
+RIGHT = 2
+UP = 3
+DOWN = 4
 going = 1
 
 screen_x = 1450
 screen_y = 800
 boundary_left = 725
 boundary_up = -400
-boundary_right = -1560
+boundary_right = -1550
 boundary_down = -1200
 screen = pygame.display.set_mode((screen_x,screen_y)) #sets the display screen
 # set display color as ocean blue
@@ -56,29 +61,31 @@ while going:
     # if e.type == pygame.KEYDOWN:
     #     # move the balloon around, depending on the keys.
     if keys[K_LEFT]:
-        print(trash_rect.x)
+        keystroke = LEFT
+        # print(trash_rect.x)
         if trash_rect.x +3 <= 0:
-            trash_rect.x += 10
+            trash_rect.x += 3
         #turtle = load_image("turtle_left.png")
     if keys[K_RIGHT]:
-        print(trash_rect.x)
-
+        # print(trash_rect.x)
+        keystroke = RIGHT
         if trash_rect.x -3 >= boundary_right:
-            trash_rect.x -= 10
+            trash_rect.x -= 3
 
         #turtle = load_image("turtle_right.png")
 
     if keys[K_UP]:
-        print(trash_rect.y)
-
+        # print(trash_rect.y)
+        keystroke = UP
         if trash_rect.y +3 <= 0:
-            trash_rect.y += 10
+            trash_rect.y += 3
 
     if keys[K_DOWN]:
-        print(trash_rect.y)
+        # print(trash_rect.y)
+        keystroke = DOWN
 
         if trash_rect.y -3 >= boundary_down:
-            trash_rect.y -= 10
+            trash_rect.y -= 3
 
     # see how far the balloon rect is offset from the terrain rect.
     bx, by = (trash_rect[0], trash_rect[1])
@@ -91,6 +98,33 @@ while going:
     #
     last_bx, last_by = bx, by
 
+    if overlap:
+        # we have hit the wall!!!  oh noes!
+        #print(keystroke)
+        if keys[K_LEFT]:
+            trash_rect.x -= 1
+
+        if keys[K_RIGHT]:
+            trash_rect.x += 1
+
+        if keys[K_UP]:
+            trash_rect.y -= 1
+        if keys[K_DOWN]:
+            trash_rect.y += 1
+
+
+
+
+        # screen.fill((7,176,157))
+        # screen.blit(pygame.transform.scale(ocean, (1450,800)), (0, 0)) #scales the image to the screen size
+        #
+        # screen.blit(trash, (trash_rect[0], trash_rect[1]) )
+        #
+        # screen.blit(turtle,(screen_x/2-150,screen_y/2-100)) #draws turtle in center
+        # # draw the balloon rect, so you can see where the bounding rect would be.
+        # pygame.draw.rect(screen, (0,255,0), trash_rect, 1)
+        #screen.blit(hitsurf, (0,0))
+        #pygame.display.flip()
 
     # draw the background color, and the terrain.
     screen.fill((7,176,157))
@@ -103,9 +137,10 @@ while going:
 
     # see if there was an overlap of pixels between the balloon
     #   and the terrain.
-    if overlap:
-        # we have hit the wall!!!  oh noes!
-        screen.blit(hitsurf, (0,0))
+    pygame.display.flip()
+
+
+
         # screen.blit(turtle,(screen_x/2-150,screen_y/2-100)) #draws turtle in center
 
 
