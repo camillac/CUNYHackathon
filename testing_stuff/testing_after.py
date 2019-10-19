eaec41424f5eccca2f8d71fd8b7bdbe81dc591df
Shafali Gupta <shafaligupta31@gmail.com>
2019-10-19 15:10:00 -0400

    turtles

--- testing_stuff/testing_after.py
+++ testing_stuff/testing_after.py
@@ -1,7 +1,12 @@
 import os
 import pygame
 import math
+from math import sin
+
 from pygame.locals import *
+import time
+
+main_dir = os.path.split(os.path.abspath(__file__))[0]
 
 pygame.display.init()
 pygame.font.init()
@@ -36,6 +41,8 @@ def load_image(i):
 turtle = load_image("turtle_right.png")
 trash = load_image("map.png")
 ocean = load_image("ocean.jpg")
+ocean = pygame.transform.scale2x(ocean)
+
 
 
 # create a mask for each of them.
@@ -50,6 +57,16 @@ afont = pygame.font.Font(None, 16)
 hitsurf = afont.render("Hit!!!  Oh noes!!", 1, (255,255,255))
 boundr = afont.render("Can't move anymore", 1, (255,255,255))
 
+if screen.get_bitsize() == 8:
+    screen.set_palette(ocean.get_palette())
+else:
+    ocean = ocean.convert()
+
+anim = 0.0
+
+# mainloop
+xblocks = range(0, screen_x, 20)
+yblocks = range(0, screen_y, 20)
 
 # start the main loop.
 
@@ -58,27 +75,38 @@ while going:
     keys = pygame.key.get_pressed()
     if keys[QUIT] or keys[K_ESCAPE]:
         going = 0
-    # if e.type == pygame.KEYDOWN:
-    #     # move the balloon around, depending on the keys.
     if keys[K_LEFT]:
         keystroke = LEFT
         # print(trash_rect.x)
         if trash_rect.x +3 <= 0:
             trash_rect.x += 3
-        #turtle = load_image("turtle_left.png")
+        turtle_mask = pygame.mask.from_surface(turtle, 50)
+
+        turtle = load_image("turtle_left.png")
+        turtle_mask = pygame.mask.from_surface(turtle, 50)
+
     if keys[K_RIGHT]:
         # print(trash_rect.x)
         keystroke = RIGHT
         if trash_rect.x -3 >= boundary_right:
             trash_rect.x -= 3
+        turtle_mask = pygame.mask.from_surface(turtle, 50)
 
-        #turtle = load_image("turtle_right.png")
+        turtle = load_image("turtle_right.png")
+        turtle_mask = pygame.mask.from_surface(turtle, 50)
+
 
     if keys[K_UP]:
         # print(trash_rect.y)
         keystroke = UP
         if trash_rect.y +3 <= 0:
             trash_rect.y += 3
+        turtle_mask = pygame.mask.from_surface(turtle, 50)
+
+        turtle = load_image("turtle_up.png")
+        turtle_mask = pygame.mask.from_surface(turtle, 50)
+
+
 
     if keys[K_DOWN]:
         # print(trash_rect.y)
@@ -86,6 +114,11 @@ while going:
 
         if trash_rect.y -3 >= boundary_down:
             trash_rect.y -= 3
+        turtle_mask = pygame.mask.from_surface(turtle, 50)
+
+        turtle = load_image("turtle_down.png")
+        turtle_mask = pygame.mask.from_surface(turtle, 50)
+
 
     # see how far the balloon rect is offset from the terrain rect.
     bx, by = (trash_rect[0], trash_rect[1])
@@ -97,7 +130,15 @@ while going:
 
     #
     last_bx, last_by = bx, by
+    screen.fill((7,176,157))
 
+    # liquid function for making it liquidy
+    anim = anim + 0.1
+    for x in xblocks:
+        xpos = (x + (sin(anim + x * 0.01) * 15)) + 20
+        for y in yblocks:
+            ypos = (y + (sin(anim + y * 0.01) * 15)) + 20
+            screen.blit(ocean, (x, y), (xpos, ypos, 20, 20))
     if overlap:
         # we have hit the wall!!!  oh noes!
         #print(keystroke)
@@ -112,23 +153,7 @@ while going:
         if keys[K_DOWN]:
             trash_rect.y += 3
 
-
-
-
-        # screen.fill((7,176,157))
-        # screen.blit(pygame.transform.scale(ocean, (1450,800)), (0, 0)) #scales the image to the screen size
-        #
-        # screen.blit(trash, (trash_rect[0], trash_rect[1]) )
-        #
-        # screen.blit(turtle,(screen_x/2-150,screen_y/2-100)) #draws turtle in center
-        # # draw the balloon rect, so you can see where the bounding rect would be.
-        # pygame.draw.rect(screen, (0,255,0), trash_rect, 1)
-        #screen.blit(hitsurf, (0,0))
-        #pygame.display.flip()
-
     # draw the background color, and the terrain.
-    screen.fill((7,176,157))
-    screen.blit(pygame.transform.scale(ocean, (1450,800)), (0, 0)) #scales the image to the screen size
     screen.blit(trash, (trash_rect[0], trash_rect[1]) )
     screen.blit(turtle,(screen_x/2-150,screen_y/2-100)) #draws turtle in center
     # draw the balloon rect, so you can see where the bounding rect would be.
@@ -138,22 +163,6 @@ while going:
     # see if there was an overlap of pixels between the balloon
     #   and the terrain.
     pygame.display.flip()
-
-
-
-        # screen.blit(turtle,(screen_x/2-150,screen_y/2-100)) #draws turtle in center
-
-
-        #screen.blit(turtle,(trash_rect.x-1, trash_rect.y-1)) #draws turtle in center
-        #trash_rect.move(trash_rect.x-50, trash_rect.y-50)
-
-
-    # flip the display.
-    #pygame.display.flip()
-
-    # # limit the frame rate to 40fps.
-    # clock.tick(40)
-
-
+    time.sleep(0.01)
 
 pygame.quit()
